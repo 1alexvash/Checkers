b11ce0ebfb0ad8cd3203fce84536dff1925af64d
Alexander <ilordarkey@gmail.com>
2019-06-10 16:35:16 +0300

    Updates

---
+++ precache-manifest.6b6dfe6ad6f236b6ad66a24a090293e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1214e038161e9ddaf2d8",
+    "url": "/Checkers/static/js/runtime~main.1214e038.js"
+  },
+  {
+    "revision": "632d25fa69cc17da23a5",
+    "url": "/Checkers/static/js/main.632d25fa.chunk.js"
+  },
+  {
+    "revision": "bd6981d1c04700d45e96",
+    "url": "/Checkers/static/js/2.bd6981d1.chunk.js"
+  },
+  {
+    "revision": "632d25fa69cc17da23a5",
+    "url": "/Checkers/static/css/main.25447924.chunk.css"
+  },
+  {
+    "revision": "481239c3211847a1c41c07ee38383f75",
+    "url": "/Checkers/index.html"
+  }
+];
